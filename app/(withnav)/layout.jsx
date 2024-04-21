3ef49e81a5d644e0432039fd6c17e3f34d640bc8
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2024-04-21 05:21:56 +0200

    add loading

--- app/(withnav)/layout.jsx
+++ app/(withnav)/layout.jsx
@@ -1,20 +1,38 @@
+'use client'
 import Footer from "@/app/_components/Footer";
 import NavBar from "@/app/_components/NavBar";
 import ScrollToTopButton from "@/app/_components/ScrollToTopButton";
+import { useEffect, useState } from "react";
+import Loading from "../loading";
 
 const Layout = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); 
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <header>
-        <NavBar />
-      </header>
-      <main>
-        {children}
-      </main>
-      <ScrollToTopButton/>
-      <section className= "bg-black padding-x padding-t pb-8 ">
-        <Footer className='absolute  bottom-0'/>
-      </section>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <header>
+            <NavBar />
+          </header>
+          <main>
+            {children}
+          </main>
+          <ScrollToTopButton/>
+          <section className="bg-black padding-x padding-t pb-8">
+            <Footer className='absolute bottom-0'/>
+          </section>
+        </>
+      )}
     </>
   );
 };
