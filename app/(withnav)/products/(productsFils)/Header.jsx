5872b32835f11bc8313a020b545dfd2ab056bd21
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2024-04-21 09:57:02 +0200

    new style

--- app/(withnav)/products/(productsFils)/Header.jsx
+++ app/(withnav)/products/(productsFils)/Header.jsx
@@ -28,7 +28,7 @@ const Header = ({ setQuery, query }) => {
     value={query}
     onChange={handleSearchChange}
   />
-  <button type="submit" className=" text-black rounded-full p-2">
+  <button type="submit" className=" text-black font-bold rounded-full p-4">
     <CiSearch className="h-8 w-8" />
   </button>
 </form>
@@ -38,11 +38,11 @@ const Header = ({ setQuery, query }) => {
 
 
         <button onClick={() => setIsOpen(true)} >
-  <div className="relative py-2">
+  <div className="relative py-2 ">
     <div className="t-0 absolute left-6">
       <p className="flex h-5 w-5 items-center justify-center rounded-full bg-red-500 p-4 text-xs text-white">{cartQuantity}</p>
     </div>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="file: mt-5 h-12 w-12">
+    <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="file: mt-5 h-12 w-12 ">
       <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
     </svg>
   </div>
